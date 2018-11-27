33aaa61f59047385554d855a2e18e8a5ea4bf04b
Edouard Lopez <contact@edouard-lopez.com>
2018-11-27 09:52:20 +0100

    test static wordlist placeholder

--- integration/placeholders.specs.js
+++ integration/placeholders.specs.js
@@ -1,7 +1,9 @@
 describe('Load application', function() {
   it('display placeholder', function() {
+    cy.server({ delay: 500 });
+
     cy.visit('/');
 
-    cy.get('data-e2e="placeholder"').should('be.visible');
+    cy.get('[data-e2e="static-wordlist-placeholder"]').should('be.visible');
   });
 });
