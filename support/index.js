c3144e9c16d0fa18726d0ab08038b4d2b2a98469
Edouard Lopez <contact@edouard-lopez.com>
2018-11-27 17:03:20 +0100

    force fetch polyfill
    see parlr/lsf@56fa1d2

--- support/index.js
+++ support/index.js
@@ -25,3 +25,9 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // failing the test
   return false;
 });
+
+// Force application to polyfill Fetch API on top of XHR.
+// Required until Cypress support fetch API https://github.com/cypress-io/cypress/issues/95#issuecomment-442107615
+Cypress.on('window:before:load', win => {
+  win.fetch = null;
+});
